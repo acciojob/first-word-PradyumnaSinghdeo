65d4e254992d5676d69161d739fd042976000d8c
Pradyumna Singh Deo <87163520+PradyumnaSinghdeo@users.noreply.github.com>
2024-07-15 13:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	 if (s === "" || !s.includes(" ")) {
+        return s;
+    } 
+    
+    return s.split(" ")[0];
 }
 
 // Do not change the code below
